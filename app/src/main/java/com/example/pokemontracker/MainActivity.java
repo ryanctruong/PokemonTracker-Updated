cd0841d740aa036a155ec1200dce0ef67652afc1
Ryan Truong <ryanctruong03@gmail.com>
2023-09-25 22:38:01 -0500

    restrictions

--- app/src/main/java/com/example/pokemontracker/MainActivity.java
+++ app/src/main/java/com/example/pokemontracker/MainActivity.java
@@ -2,27 +2,70 @@ package com.example.pokemontracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
+import java.util.*;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    View.OnClickListener resetListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            et_nationalNumber.setText(getString(R.string._896));
+            et_name.setText(getString(R.string.glastrier));
+            et_species.setText(getString(R.string.wild_horse_pokemon));
+            et_height.setText(getString(R.string._2_2_m));
+            et_weight.setText(getString(R.string._800_0_kg));
+            et_hp.setText("0");
+            et_attack.setText("0");
+            et_defence.setText("0");
+            splevel.setSelection(0);
+        }
+    };
+
+    View.OnClickListener saveListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (validateInputs()) {
+                Toast.makeText(getApplicationContext(), "Information stored in the database", Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    AdapterView.OnItemSelectedListener spinListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            String message = adapterView.getItemAtPosition(i).toString();
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    };
     TextView appName, regisCatch, nationalNumber, name, species, gender, height, weight,
-            level, hp, attack, defence;
+            level, baseStats, hp, attack, defence;
 
     EditText et_nationalNumber, et_name, et_species, et_height, et_weight,
             et_hp, et_attack, et_defence;
     RadioButton female, male, unk;
 
     Button reset, save;
+    Spinner splevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Titles
         appName = requireViewById(R.id.AppName);
         regisCatch = requireViewById(R.id.newCatch);
+        baseStats = requireViewById(R.id.baseStats);
 
         // editText headers
         nationalNumber = requireViewById(R.id.natNumber);
@@ -51,9 +94,104 @@ public class MainActivity extends AppCompatActivity {
         male = requireViewById(R.id.radioMale);
         unk = requireViewById(R.id.radioUnk);
 
-
         //Buttons
         reset = requireViewById(R.id.resetButton);
         save = requireViewById(R.id.saveButton);
+
+        //Spinner
+        splevel = requireViewById(R.id.spinLevel);
+        splevel.setOnItemSelectedListener(spinListener);
+
+        ArrayList<Integer> valuesList = new ArrayList<>();
+        for(int i = 1; i <= 50; i++){
+            valuesList.add(i);
+        }
+
+        ArrayAdapter<Integer> typeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,valuesList);
+        splevel.setAdapter(typeAdapter);
+
+        reset.setOnClickListener(resetListener);
+        save.setOnClickListener(saveListener);
+    }
+
+    private boolean validateInputs() {
+        boolean allValid = true;
+
+        nationalNumber.setTextColor(Color.parseColor("Black"));
+        name.setTextColor(Color.parseColor("Black"));
+        species.setTextColor(Color.parseColor("Black"));
+        gender.setTextColor(Color.parseColor("Black"));
+
+        hp.setTextColor(Color.parseColor("Black"));
+        attack.setTextColor(Color.parseColor("Black"));
+        defence.setTextColor(Color.parseColor("Black"));
+
+
+        if (et_nationalNumber.getText().toString().trim().isEmpty() ||
+                et_name.getText().toString().trim().isEmpty() ||
+                et_species.getText().toString().trim().isEmpty() ||
+                et_height.getText().toString().trim().isEmpty() ||
+                et_weight.getText().toString().trim().isEmpty() ||
+                et_hp.getText().toString().trim().isEmpty() ||
+                et_attack.getText().toString().trim().isEmpty() ||
+                et_defence.getText().toString().trim().isEmpty()) {
+            allValid = false;
+            Toast.makeText(getApplicationContext(), "Please fill all fields.", Toast.LENGTH_SHORT).show();
+        }
+
+        int nNumberValue = Integer.parseInt(et_nationalNumber.getText().toString().trim());
+        if(nNumberValue < 0 || nNumberValue > 1010){
+            Toast.makeText(getApplicationContext(), "Number must be between 0-1010.", Toast.LENGTH_SHORT).show();
+            nationalNumber.setTextColor(Color.parseColor("Red"));
+            allValid = false;
+        }
+
+        //need to figure out a way to limit to just characters and dots and spaces
+        String nameInput = et_name.getText().toString().trim();
+        if (nameInput.length() < 3 || nameInput.length() > 12 || !nameInput.matches("[a-zA-Z]+")) {
+            Toast.makeText(getApplicationContext(), "Name length must be 3-12 characters.", Toast.LENGTH_SHORT).show();
+            name.setTextColor(Color.parseColor("Red"));
+            allValid = false;
+        }
+
+        //need to figure out a way to limit to just characters and dots and spaces
+        String speciesInput = et_species.getText().toString().trim();
+
+
+        boolean isMale = male.isChecked();
+        boolean isFemale = female.isChecked();
+        boolean isUnk  = unk.isChecked();
+        if (!isMale && !isFemale && isUnk) {
+            Toast.makeText(getApplicationContext(), "Please select a gender.", Toast.LENGTH_SHORT).show();
+            gender.setTextColor(Color.parseColor("Red"));
+            allValid = false;
+        }
+
+        int hpValue = Integer.parseInt(et_hp.getText().toString().trim());
+        if (hpValue < 1 || hpValue > 362) {
+            Toast.makeText(getApplicationContext(), "HP must be 1 to 362.", Toast.LENGTH_SHORT).show();
+            hp.setTextColor(Color.parseColor("Red"));
+
+            allValid = false;
+        }
+
+        int attackValue = Integer.parseInt(et_attack.getText().toString().trim());
+        if (attackValue < 5 || attackValue > 526) {
+            Toast.makeText(getApplicationContext(), "Attack must be 5 to 562.", Toast.LENGTH_SHORT).show();
+            attack.setTextColor(Color.parseColor("Red"));
+
+            allValid = false;
+        }
+
+
+        int defenceValue = Integer.parseInt(et_defence.getText().toString().trim());
+        if (defenceValue < 5 || defenceValue > 614) {
+            Toast.makeText(getApplicationContext(), "Defence must be 5 to 614.", Toast.LENGTH_SHORT).show();
+            defence.setTextColor(Color.parseColor("Red"));
+
+            allValid = false;
+        }
+
+        return allValid;
     }
 }
